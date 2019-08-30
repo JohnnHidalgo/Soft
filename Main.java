91c6ab5e0deb8d991ba583a34c5591d458f1e00d
Mauricio Celis <mau.armata@gmail.com>
2019-08-29 21:10:37 -0400

    Add primo function

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Soft {
 
 	public static void main (String []args) {
-		fibonaci();
-		primo();
+		fibonaci(5);
+		esprimo(5);
 		cuadrado();
 	}
 	public int fibonaci(int n){
@@ -13,5 +13,26 @@ public class Soft {
 	    return fibonaci(n-1)+fibonaci(n-2);
 	  }
 	}
+
+
+Public void esprimo(int num){
+	int cont=0;
+	int p=1;
+	for(int i=1;i<=num;i++){
+		if(num % i ==0){
+			cont++;
+		}
+	} 
+	
+	if(cont==2){
+		System.out.println(“Es primo”)
+	
+	}
+	else{
+		System.out.println(“No es primo”)
+	
+	}
+
+}
 
 }
