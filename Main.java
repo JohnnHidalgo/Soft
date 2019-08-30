d30dc91b7fee76d2f65c9af46d0073d1c3a635e6
Johnn Hidalgo <Johnn@gmail.com>
2019-08-29 21:03:26 -0400

    Add fibonaci function

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 public class Soft {
 
 	public static void main (String []args) {
-		fibo();
+		fibonaci();
 		primo();
 		cuadrado();
+	}
+	public int fibonaci(int n){
+	  if(n==1 || n==2) {
+	    return 1;
+	  }
+	  else{
+	    return fibonaci(n-1)+fibonaci(n-2);
+	  }
 	}
 
 }
